bbc7b268c7d97c13aaea4290c94ae4e4b7550c69
Thomas4897 <t.maynard4897@gmail.com>
2021-10-20 11:03:33 +0800

    Adding First Functions

--- main.js
+++ main.js
@@ -1,59 +1,108 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(x, y) {
+	let output = false;
+	if (x % y === 0) {
+		output = true;
+	}
+
+	return output;
 }
 
-function halfSquare() {
+function halfSquare(x) {
+	return x ** 2 / 2;
 }
 
-function isLong() {
+function isLong(str) {
+	let output = false;
+
+	if (str.length >= 15) {
+		output = true;
+	}
+
+	return output;
 }
 
-function exclaim() {
+function exclaim(str) {
+	let count = 0;
+	let output = "";
+
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === "!") {
+			count++;
+		}
+	}
+
+	for (let i = 0; i < str.length - count; i++) {
+		output += str[i];
+	}
+
+	return output + "!";
 }
 
-function countWords() {
+function countWords(str) {
+	let strArr = str.split(" ");
+
+	return strArr.length;
 }
 
-function containsDigit() {
+function containsDigit(str) {
+	let output = false;
+
+	for (let i = 0; i < str.length; i++) {
+		const isLetter = isNaN(str[i]);
+		if (isLetter === false && str[i] !== " ") {
+			output = true;
+		}
+	}
+	return output;
 }
 
-function containsLowerCase() {
+function containsLowerCase(str) {
+	return str.toUpperCase() != str;
 }
 
-function containsUpperCase() {
+function containsUpperCase(str) {
+	return str.toLowerCase() != str;
 }
 
-function containsNonAlphanumeric() {
+function containsNonAlphanumeric(str) {
+	let output = false;
+
+	for (let i = 0; i < str.length; i++) {
+		const isLetter = isNaN(str[i]);
+		if (
+			isLetter === true ||
+			(isLetter === false && isLetter === true) ||
+			str[i] === " "
+		) {
+			output = true;
+		}
+	}
+	return output;
 }
 
-function containsSpace() {
-}
+function containsSpace() {}
 
-function digits() {
-}
+function digits() {}
 
-function truncate() {
-}
+function truncate() {}
 
-function isValidPassword() {
-}
+function isValidPassword() {}
 
-function onlyPunchy() {
-}
-
+function onlyPunchy() {}
 
 module.exports = {
-  isEvenlyDivisible,
-  halfSquare,
-  exclaim,
-  isLong,
-  containsDigit,
-  containsLowerCase,
-  containsUpperCase,
-  containsNonAlphanumeric,
-  containsSpace,
-  countWords,
-  digits,
-  truncate,
-  isValidPassword,
-  onlyPunchy,
-}
+	isEvenlyDivisible,
+	halfSquare,
+	exclaim,
+	isLong,
+	containsDigit,
+	containsLowerCase,
+	containsUpperCase,
+	containsNonAlphanumeric,
+	containsSpace,
+	countWords,
+	digits,
+	truncate,
+	isValidPassword,
+	onlyPunchy,
+};
